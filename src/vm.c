b5542a39d9d5129dbcc55cd519de2a75e6f3d8b8
Alex Dauncey <alexanderdauncey@gmail.com>
2022-09-03 22:20:41 +0100

    Value Stack Manipulation

--- src/vm.c
+++ src/vm.c
@@ -3,14 +3,37 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "value.h"
 #include "vm.h"
 
 VM vm;
 
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
 void initVM() {
+  resetStack();
 }
 
 void freeVM() {
+}
+
+void push(Value value) {
+  // Stores value in the array element at the top of the stack
+  // (Points at the next empty element)
+  *vm.stackTop = value;
+  // Increment the pointer to the new next empty element
+  vm.stackTop++;
+}
+
+Value pop() {
+  // Move back to the last element
+  vm.stackTop--;
+  // Return the last element (now pointing at next slot)
+  // Dont need to free anything ahead as the array is already
+  // full in use, stackTop just finds the next place we can write to
+  return *vm.stackTop;
 }
 
 static InterpretResult run() {
@@ -21,8 +44,16 @@ static InterpretResult run() {
 
   // Each iteration of this loop reads and executes 1 bytecode instruction
   for (;;) {
-// Print instructions as they are executed
 #ifdef DEBUG_TRACE_EXECUTION
+// Print stack before each instruction (stack trace)
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+// Print instructions as they are executed
     disassembleInstruction(vm.chunk, 
                             (int)(vm.ip - vm.chunk->code));
 #endif
@@ -30,11 +61,12 @@ static InterpretResult run() {
     switch (instruction = READ_BYTE()) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
       case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
       }
     }
